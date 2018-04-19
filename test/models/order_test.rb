b5dd3f14b8c19a4336119ab85090dddea99730b0
Alexandria Brown <axbn2190@gmail.com>
2018-04-19 15:50:00 -0700

    merging conficts

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,10 +1,9 @@
 require "test_helper"
 
 describe Order do
-  let(:order) { Order.new address: "123, Fake address", card_name: "A name", cc_number: "1234567891234567", cc_expiration: Date.new(2001,2) , cvv: "123", zip_code: "98005", email: "email@test.com" }
+  let(:order) { Order.new address: "123, Fake address", card_name: "A name", cc_number: "1234567891234567", cc_expiration: Date.new(2001,2), cvv: "123", zip_code: "98005", email: "email@test.com" }
 
   describe 'validations' do
-
     it "must be valid" do
       value(order).must_be :valid?
     end
