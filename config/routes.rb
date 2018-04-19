22567688006e8148cabfcfbfc1d8a86e1b5c8930
Alexandria Brown <axbn2190@gmail.com>
2018-04-19 16:39:21 -0700

    orders controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+
+  get 'orders/show'
+
+  get 'orders/new'
+
+  get 'orders/create'
+
+  get 'orders/update'
+
+  get 'orders/edit'
+
+  get 'orders/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
