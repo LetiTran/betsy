8d5ddefd65878e51a33bf85d248f066e26d04f2e
Aruna <aruna.thungathurthi@gmail.com>
2018-04-19 16:39:38 -0700

    added merchants controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'merchants/index'
+
+  get 'merchants/show'
+
+  get 'merchants/new'
+
+  get 'merchants/create'
+
+  get 'merchants/edit'
+
+  get 'merchants/update'
+
+  get 'merchants/destroy'
+
   get 'products/index'
 
   get 'products/show'
