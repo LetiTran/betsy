dd9e45cf0e26703be9a6a48dbec51a25c5f51a11
LetiTran <leticiastran@gmail.com>
2018-04-19 16:39:19 -0700

    Genereted categories_controller and reviews_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+
+  get 'reviews/show'
+
+  get 'reviews/new'
+
+  get 'reviews/create'
+
+  get 'reviews/update'
+
+  get 'reviews/edit'
+
+  get 'reviews/destroy'
+
+  get 'categories/index'
+
+  get 'categories/show'
+
+  get 'categories/new'
+
+  get 'categories/create'
+
+  get 'categories/update'
+
+  get 'categories/edit'
+
+  get 'categories/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
