95ad00280ff835079de1beea3e429fa21243a45f
LetiTran <leticiastran@gmail.com>
2018-04-19 15:47:24 -0700

    Added relations test for product.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -3,63 +3,91 @@ require "test_helper"
 describe Product do
   let(:product) { Product.new name: "thing", price: 1, category: "red", quantity: 2 }
 
-  it "must be valid" do
-    product.must_be :valid?
+  describe 'Validations' do
+
+    it "must be valid" do
+      product.must_be :valid?
+    end
+
+    it "must have a name" do
+      product.name = nil
+
+      product.valid?.must_equal false
+      product.errors.must_include :name
+    end
+
+    it "must have name length greater than zero" do
+      product.name = ""
+
+      product.valid?.must_equal false
+      product.errors.must_include :name
+    end
+
+    it "must have a category" do
+      product.category = nil
+
+      product.valid?.must_equal false
+      product.errors.must_include :category
+    end
+
+    it "must have category length greater than zero" do
+      product.category = ""
+
+      product.valid?.must_equal false
+      product.errors.must_include :category
+    end
+
+    it "must have a quantity" do
+      product.quantity = nil
+
+      product.valid?.must_equal false
+      product.errors.must_include :quantity
+    end
+
+    it "must not have a quantity less than zero" do
+      product.quantity = -1
+
+      product.valid?.must_equal false
+      product.errors.must_include :quantity
+    end
+
+    it "must have a price" do
+      product.price = nil
+
+      product.valid?.must_equal false
+      product.errors.must_include :price
+    end
+
+    it "must not have a price less than zero" do
+      product.price = -2
+
+      product.valid?.must_equal false
+      product.errors.must_include :price
+    end
   end
 
-  it "must have a name" do
-    product.name = nil
-
-    product.valid?.must_equal false
-    product.errors.must_include :name
+describe 'relations' do
+  it "has a list of orders" do
+    product.must_respond_to :orders
+    product.orders.each do |order|
+      order.must_be_kind_of Order
+    end
   end
 
-  it "must have name length greater than zero" do
-    product.name = ""
-
-    product.valid?.must_equal false
-    product.errors.must_include :name
+  it "has a list of categories" do
+    product.must_respond_to :categories
+    product.categories.each do |category|
+      category.must_be_kind_of Category
+    end
   end
 
-  it "must have a category" do
-    product.category = nil
-
-    product.valid?.must_equal false
-    product.errors.must_include :category
+  it "has a list of reviews" do
+    product.must_respond_to :reviews
+    product.reviews.each do |review|
+      review.must_be_kind_of Review
+    end
   end
 
-  it "must have category length greater than zero" do
-    product.category = ""
+end
 
-    product.valid?.must_equal false
-    product.errors.must_include :category
-  end
-
-  it "must have a quantity" do
-    product.quantity = nil
-
-    product.valid?.must_equal false
-    product.errors.must_include :quantity
-  end
-
-  it "must not have a quantity less than zero" do
-    product.quantity = -1
-
-    product.valid?.must_equal false
-    product.errors.must_include :quantity
-  end
-
-  it "must have a price" do
-    product.price = nil
-
-    product.valid?.must_equal false
-    product.errors.must_include :price
-  end
-
-  it "must not have a price less than zero" do
-    product.price = -2
-
-    product.valid?.must_equal false
-    product.errors.must_include :price
-  end
 end
