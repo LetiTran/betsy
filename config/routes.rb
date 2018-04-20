add2ff7dfb52a3b58cb09b927770938b12c7a8fe
Alexandria Brown <axbn2190@gmail.com>
2018-04-20 10:58:15 -0700

    nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,104 +2,31 @@ Rails.application.routes.draw do
 
   # Products:
   root 'products#root', as: 'homepage'
-
   resources :products do
-    resouces :reviews, only: [:new]
+    # add orderproducts (cart#index)
+    resources :reviews, only: [:new]
+    resources :merchants, only: [:index]
+    resources :categories, only: [:index]
   end
 
-  # get 'products/index'
-  #
-  # get 'products/:id', to: 'products#show', as: 'product'
-  #
-  # get 'products/new'
-  #
-  # get 'products/create'
-  #
-  # get 'products/edit'
-  #
-  # get 'products/update'
-  #
-  # get 'products/destroy'
-
   # Merchants:
-  resources :merchants
-  # get 'merchants/index'
-  #
-  # get 'merchants/show'
-  #
-  # get 'merchants/new'
-  #
-  # get 'merchants/create'
-  #
-  # get 'merchants/edit'
-  #
-  # get 'merchants/update'
-  #
-  # get 'merchants/destroy'
+  resources :merchants do
+    resources :products, only: [:index]
+  end
 
   # Sessions:
   get '/login', to: 'sessions#new', as: 'login_form'
   post '/login', to: 'sessions#create'
   delete '/login', to: 'sessions#destroy', as: 'logout'
-
   resources :sessions, only: [:index, :show]
-  # get 'sessions/index'
-  #
-  # get 'sessions/show'
-
-  # get 'sessions/new'
-
-  # get 'sessions/create'
-
-  # get 'sessions/destroy'
 
   # Reviews:
   resources :reviews
-  # get 'reviews/index'
-  #
-  # get 'reviews/show'
-  #
-  # get 'reviews/new'
-  #
-  # get 'reviews/create'
-  #
-  # get 'reviews/update'
-  #
-  # get 'reviews/edit'
-  #
-  # get 'reviews/destroy'
 
   # Categories:
   resources :categories
-  # get 'categories/index'
-  #
-  # get 'categories/show'
-  #
-  # get 'categories/new'
-  #
-  # get 'categories/create'
-  #
-  # get 'categories/update'
-  #
-  # get 'categories/edit'
-  #
-  # get 'categories/destroy'
 
   # Orders:
   resources :orders
-  # get 'orders/index'
-  #
-  # get 'orders/show'
-  #
-  # get 'orders/new'
-  #
-  # get 'orders/create'
-  #
-  # get 'orders/update'
-  #
-  # get 'orders/edit'
-  #
-  # get 'orders/destroy'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
