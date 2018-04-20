0e3c4909f9da44cb41bcd34de053f048002f4e1d
Alexandria Brown <axbn2190@gmail.com>
2018-04-19 22:12:48 -0700

    model testing style standardizing

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -6,146 +6,194 @@ describe Order do
 
   describe 'validations' do
     it "must be valid" do
-      value(order).must_be :valid?
+      order.must_be :valid?
     end
 
     it 'must have a address' do
       order.address = nil
+
       order.valid?.must_equal false
+      order.errors.must_include :address
     end
 
     it 'must have an address with length more than 0 characters' do
       order.address = ""
+
       order.valid?.must_equal false
+      order.errors.must_include :address
     end
 
     it 'must cannot have an address with length more than 35' do
       order.address = "123451234512345123451234512345123451"
+
       order.valid?.must_equal false
+      order.errors.must_include :address
     end
 
     it 'must have a card name' do
       order.card_name = nil
+
       order.valid?.must_equal false
+      order.errors.must_include :card_name
     end
 
     it 'must have a card name with more than 0 characters' do
       order.card_name = ""
+
       order.valid?.must_equal false
+      order.errors.must_include :card_name
     end
 
     it 'must have a card number' do
       order.cc_number = nil
+
       order.valid?.must_equal false
+      order.errors.must_include :cc_number
     end
 
     it 'must have a card expiration' do
       order.cc_expiration = nil
+
       order.valid?.must_equal false
+      order.errors.must_include :cc_expiration
     end
 
     it 'card expiration must be a date' do
       order.cc_expiration = "a string"
+
       order.valid?.must_equal false
+      order.errors.must_include :cc_expiration
     end
 
     it 'must have a cvv' do
       order.cvv = nil
+
       order.valid?.must_equal false
+      order.errors.must_include :cvv
     end
 
     it "must have a length of three characters" do
       order.cvv = "cvvv"
+
       order.valid?.must_equal false
+      order.errors.must_include :cvv
     end
 
     it 'must have a zipcode' do
       order.zip_code = nil
+
       order.valid?.must_equal false
+      order.errors.must_include :zip_code
     end
 
     it "zipcode length must be 5 " do
       order.zip_code = "980756"
+
       order.valid?.must_equal false
+      order.errors.must_include :zip_code
     end
 
     it 'must have a email' do
       order.email = nil
+
       order.valid?.must_equal false
+      order.errors.must_include :email
     end
 
     it "must have a valid email format" do
       order.email = "12345@.email.com"
+
       order.valid?.must_equal false
+      order.errors.must_include :email
     end
 
     it "must have a unique email id" do
       order.email = "some_person@testing.com"
+
       order.valid?.must_equal false
+      order.errors.must_include :email
     end
     it 'must have a address' do
       order.address = nil
+
       order.valid?.must_equal false
+      order.errors.must_include :address
     end
 
     it 'must have a address with length more' do
       order.address = nil
+
       order.valid?.must_equal false
+      order.errors.must_include :address
     end
 
     it 'must have a card name' do
       order.card_name = nil
-      order.valid?.must_equal false
-    end
 
-    it 'must have a card number' do
-      order.cc_number = nil
       order.valid?.must_equal false
+      order.errors.must_include :card_name
     end
 
     it 'must have a card expiration' do
       order.cc_expiration = nil
+
       order.valid?.must_equal false
+      order.errors.must_include :cc_expiration
     end
 
     it 'must have a cvv' do
       order.cvv = nil
+
       order.valid?.must_equal false
+      order.errors.must_include :cvv
     end
     it "must have a length of three characters" do
       order.cvv = "cvvv"
+
       order.valid?.must_equal false
+      order.errors.must_include :cvv
     end
 
     it 'must have a zipcode' do
       order.zip_code = nil
+
       order.valid?.must_equal false
+      order.errors.must_include :zip_code
     end
 
     it "zipcode length must be 5 " do
       order.zip_code = "980756"
+
       order.valid?.must_equal false
+      order.errors.must_include :zip_code
     end
 
     it 'must have a email' do
       order.email = nil
+
       order.valid?.must_equal false
+      order.errors.must_include :email
     end
 
     it "must have a valid email format" do
       order.email = "12345@.email.com"
+
       order.valid?.must_equal false
+      order.errors.must_include :email
     end
 
     it "must have a unique email id" do
       order.email = "some_person@testing.com"
+
       order.valid?.must_equal false
+      order.errors.must_include :email
     end
   end
 
   describe "relations" do
     it "has a list of products" do
       order.must_respond_to :products
+
       order.products.each do |product|
         product.must_be_kind_of Product
       end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -3,41 +3,43 @@ require "test_helper"
 describe Review do
   let(:review) { Review.new rating:1, comment:"hdjdjhjj" }
 
-  it "must be valid" do
-    review.product = products(:candy)
-    review.valid?.must_equal true
+  describe "validations" do
+    it "must be valid" do
+      review.product = products(:candy)
+
+      review.valid?.must_equal true
+    end
+
+    it "must be have a rating" do
+      review.rating = nil
+
+      review.valid?.must_equal false
+      review.errors.must_include :rating
+    end
+
+    it "rating must be between 1 and 5" do
+      review.rating = 6
+
+      review.valid?.must_equal false
+      review.errors.must_include :rating
+    end
+
+    it "must have a comment between 1 and 250 characters" do
+      review.comment = ""
+
+      review.valid?.must_equal false
+      review.errors.must_include :comment
+    end
   end
 
-  it "must be have a rating" do
+  describe "relations" do
+    it "belongs to a product and creates an error message if no product is given" do
+      review = Review.new(rating: 1, comment:"qwewe")
+      review.must_respond_to :product
 
-    review.rating = nil
-    review.valid?.must_equal false
-  end
-
-  it "rating must be between 1 and 5" do
-
-    review.rating = 6
-    review.valid?.must_equal false
-  end
-
-  it "must have a comment between 1 and 250 characters" do
-
-    review.comment = ""
-    review.valid?.must_equal false
-  end
-
-  it "belongs to a product and creates an error message if no product is given" do
-
-<<<<<<< HEAD
-=======
-  it "belongs to a product and creates an error message if no product is given" do
-
->>>>>>> 4abfedf6e5f3b151bcc59d057c836b4ac4634c6e
-    review = Review.new(rating: 1, comment:"qwewe")
-    review.must_respond_to :product
-
-    review.wont_be :valid?
-    review.errors.messages.must_include :product
+      review.wont_be :valid?
+      review.errors.messages.must_include :product
+    end
   end
 
 end
