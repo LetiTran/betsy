4ac83ad4cc96b2c844b2339f05d51c6115f951d4
Alexandria Brown <axbn2190@gmail.com>
2018-04-23 09:29:16 -0700

    index and show

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -4,37 +4,28 @@ describe MerchantsController do
   it "should get index" do
     get merchants_path
 
-    must_respond_with :success?
+    must_respond_with :success
   end
 
   it "should get show" do
     get merchant_path(merchants(:one).id)
-    value(response).must_be :success?
+
+    must_respond_with :success
   end
 
   it "should get new" do
-    get merchants_new_url
-    value(response).must_be :success?
   end
 
   it "should get create" do
-    get merchants_create_url
-    value(response).must_be :success?
   end
 
   it "should get edit" do
-    get merchants_edit_url
-    value(response).must_be :success?
   end
 
   it "should get update" do
-    get merchants_update_url
-    value(response).must_be :success?
   end
 
   it "should get destroy" do
-    get merchants_destroy_url
-    value(response).must_be :success?
   end
 
 end
