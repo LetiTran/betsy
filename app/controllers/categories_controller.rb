6824e1522012fd074ea26e2642ddfcad9ab3f6a9
Alexandria Brown <axbn2190@gmail.com>
2018-04-25 21:45:51 -0700

    styling

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
   before_action :find_category, only: [:show, :edit, :update, :destroy]
   before_action :find_user
-  
+
   def index
     @categories = Category.all
   end
@@ -17,8 +17,12 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
     if @category.save
       flash[:status] = :success
-      flash[:result_text] = "Successfully created #{@category.name}"
-      redirect_to categories_path(@category.id)
+      flash[:result_text] = "Successfully added #{@category.name}"
+
+      redirect_to
+      new_product_path
+
+      # categories_path(@category.id)
     else
       flash[:status] = :failure
       flash[:result_text] = "Could not create this category"
