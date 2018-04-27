c6c4ac0ff7c8ffec1201d3a889f321f8574def37
Aruna <aruna.thungathurthi@gmail.com>
2018-04-27 09:10:45 -0700

    updated validations in order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,18 +4,18 @@ class Order < ApplicationRecord
   has_many :orderproducts
   has_many :products, through: :orderproducts
 
-  with_options if: :checking_out? do |order|
-    validates :address, presence: true, length: { in: 1..35  }
-    validates :card_name, presence: true, length: { minimum: 1  }
-    validates :cc_number, presence: true, length: { is: 16 }
-    validates :cc_expiration, presence: true
-    # TODO: the date should be grater than today
-    validates :cvv, presence: true, length: { is: 3 }
-    validates :zip_code, presence: true, length: { is: 5 }
-    validates :email, presence: true,
-    format: { with:  VALID_EMAIL },
-    uniqueness: { case_sensitive: false }
-   end
+  # with_options if: :checking_out? do |order|
+  #   validates :address, presence: true, length: { in: 1..35  }
+  #   validates :card_name, presence: true, length: { minimum: 1  }
+  #   validates :cc_number, presence: true, length: { is: 16 }
+  #   validates :cc_expiration, presence: true
+  #   # TODO: the date should be grater than today
+  #   validates :cvv, presence: true, length: { is: 3 }
+  #   validates :zip_code, presence: true, length: { is: 5 }
+  #   validates :email, presence: true,
+  #   format: { with:  VALID_EMAIL },
+  #   uniqueness: { case_sensitive: false }
+  #  end
 
   def checking_out?
     :status == "open"
