e35415d676adef4d16ad443dad5fe9739c3898b7
Alexandria Brown <axbn2190@gmail.com>
2018-04-27 13:49:28 -0700

    photo

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -60,7 +60,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    return params.require(:product).permit(:name, :price, :quantity,:status, :description, :category_ids => [], :status, :photo)
+    return params.require(:product).permit(:name, :price, :quantity, :status, :photo, :description, :category_ids => [])
   end
 
   def find_active_products
