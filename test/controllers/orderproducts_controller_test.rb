c82e35647c84c9048f376a82edb3bdbdf9b27741
Alexandria Brown <axbn2190@gmail.com>
2018-04-27 17:28:22 -0700

    more testing

--- test/controllers/orderproducts_controller_test.rb
+++ test/controllers/orderproducts_controller_test.rb
@@ -2,49 +2,81 @@ require "test_helper"
 
 describe OrderproductsController do
   it "should get index" do
-    get orderproducts_index_url
-    value(response).must_be :success?
+    get orderproducts_path
+
+    must_respond_with :success
   end
 
   it "should get show" do
-    get orderproducts_show_url
-    value(response).must_be :success?
+    get orderproduct_path(Orderproduct.first.id)
+
+    must_respond_with :success
   end
 
-  it "should get create" do
-    get orderproducts_create_url
-    value(response).must_be :success?
+  describe "create" do
+    it "should increase ordperproduct count" do
+      before = Orderproduct.all.count
+
+      newop = Orderproduct.create_orderproduct(products(:candy).quantity, products(:candy).id, orders(:open).id)
+      newop.save
+
+      Orderproduct.all.count.must_equal before +1
+    end
   end
 
   it "should get new" do
-    get orderproducts_new_url
-    value(response).must_be :success?
+    get new_orderproduct_path
+
+    must_respond_with :success
   end
 
   it "should get edit" do
-    get orderproducts_edit_url
-    value(response).must_be :success?
+    get edit_orderproduct_path(Orderproduct.last.id)
+    get edit_orderproduct_path(Orderproduct.first.id)
+
+    must_respond_with :success
   end
 
-  it "should get update" do
-    get orderproducts_update_url
-    value(response).must_be :success?
+  describe "update" do
+    it "successfully update field with valid id" do
+      updated_quantity = 6
+
+      patch orderproduct_path(Orderproduct.first.id),
+      params: {
+        orderproduct: {
+          quantity: updated_quantity,
+        }
+      }
+
+      updated_op = Orderproduct.find(Orderproduct.first.id)
+      updated_op.quantity.must_equal updated_quantity
+    end
   end
 
-  it "should get destroy" do
-    get orderproducts_destroy_url
-    value(response).must_be :success?
+  describe "destroy" do
+    it "deletes a orderproduct" do
+      delete orderproduct_path(Orderproduct.first.id)
+
+      must_respond_with :redirect
+    end
+
+    it "reduces orderproduct count" do
+      initial = Orderproduct.count
+
+      delete orderproduct_path(Orderproduct.first.id)
+
+      Orderproduct.count.must_equal initial - 1
+
+    end
   end
 
-
-  # 
-  # it "must increase orderproduct count" do
-  #   before = Orderproduct.all.count
-  #
-  #   newop = Orderproduct.create_orderproduct(products(:candy).quantity, products(:candy).id, orders(:open).id)
-  #   newop.save
-  #
-  #   Orderproduct.all.count.must_equal before +1
-  # end
+  describe "clear cart" do
+    # it "should delete orderproducts from order" do
+    #   orderproducts(:one).clear_cart
+    #
+    #   must_respond_with :redirect
+    #   must_redirect_to orderproducts_path
+    # end
+  end
 
 end
